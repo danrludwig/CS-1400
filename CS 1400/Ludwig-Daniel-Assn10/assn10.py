b58cbd2400ab7761d8bd081eb5b4ceb178840a79
Daniel Ludwig <47536087+danrludwig@users.noreply.github.com>
2019-04-04 15:00:40 -0600

    Update assn10.py

--- CS 1400/Ludwig-Daniel-Assn10/assn10.py
+++ CS 1400/Ludwig-Daniel-Assn10/assn10.py
@@ -1,20 +1,14 @@
-'''
-    Daniel Ludwig   A02220549
-    Assn 10
-    CS 1400
-'''
 
-#### Add Import Statement(s) as needed ####
 import turtle
 from chessboard import drawChessboard
-#### End Add Import Statement(s) ####
+
 
 def main():
-    #### Add Code to get input from user ####
+  
     startX, startY = eval(input('Enter at start positions: '))
     width = input('Enter the width: ')
     height = input('Enter the height: ')
-    #### End Add Code to get input from user ####
+
 
     if width == "" and height == "":
         drawChessboard(startX, startY)
